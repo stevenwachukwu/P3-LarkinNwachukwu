f846305b69248d737083745d98aba1692d5906f4
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-02-09 00:48:15 -0500

    Create dice.hpp

---
+++ dice.hpp
@@ -0,0 +1,18 @@
+#ifndef dice
+#define dice
+
+#include "tools.hpp"
+
+class Dice {
+private:
+    int nDice;
+    int* diceVal;
+
+public:
+    Dice(int n);
+    ~Dice();
+    ostream& print(ostream&);
+    const int* roll();
+};
+inline ostream& operator << (ostream& output, Dice& d) {return d.print(output);}
+#endif
