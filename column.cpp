4f7685dcb5487bda9c06d10919c676f45791c12b
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-02-09 20:20:22 -0500

    Update column.cpp

--- column.cpp
+++ column.cpp
@@ -1,13 +1,23 @@
 
 #include "column.hpp"
 
+Column::Column(int columnNum):columnNum(columnNum) {}
+
+Column::~Column(){}
+
 int Column::getState() {
     return state;
 }
 
 ostream& Column::print (ostream& columnOutput) {
-columnOutput << columnNum << colstates[state] << endl;
+columnOutput << columnNum << " " << colstates[state] << " T";
 for(int k = 0; k < columnVal [columnNum]; k++) {
-    columnOutput << "--- ----- ---- ----- ----";
+    columnOutput << "-";
+    for(int j = 0; j < 5; j++) {
+           if(columnMarker[j] == k) {
+               columnOutput << MarkerColorStrings[j];
+           }
+        }
     }
+    return columnOutput;
 }
