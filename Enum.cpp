2b0525cc422c4fb6097cd5be7d3c3f2075fc7c3b
Steve Nwachukwu <144286017+stevenwachukwu@users.noreply.github.com>
2024-02-14 17:21:45 -0500

    Update Enum.cpp

--- Enum.cpp
+++ Enum.cpp
@@ -1,28 +1,5 @@
+/*Written by Steve Nwachukwu and Nick Larkin*/
 #include "Enum.hpp"
-#include "tools.hpp"
-#include <iostream>
-// Define the array of strings for enum output
-//const string colorStrings[] = { "white", "orange", "yellow", "green", "blue"/*, "red" */ };
+const string colorStrings[] = { "White", "Orange", "Yellow", "Green", "Blue", "Red"};
 
-// Function to input ECcolor enum
-ECcolor inputColor() {
-    char choice;
-    cout << "Select a color (White,Orange,Yellow,Green,Blue): ";
-    cin >> choice;
-    switch (choice) {
-        case 'w': case 'W': return ECcolor::white;
-        case 'o': case 'O': return ECcolor::orange;
-        case 'y': case 'Y': return ECcolor::yellow;
-        case 'g': case 'G': return ECcolor::green;
-        case 'b': case 'B': return ECcolor::blue;
-        case 'r': case 'R': return ECcolor::red;
-
-        default:
-            fatal("Invalid color choice. Defaulting to white.");
-            return ECcolor::white;
-    }
-}
-//Function to convert ECcolor enum to string
-string colorToString(ECcolor color) {
-    return colorStrings[static_cast<int>(color)];
-}
+const string MarkerColorStrings[] = {"White", "Orange", "Yellow", "Green", "Blue"};
